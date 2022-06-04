c16d19c3e66cfa9d5a50b72e555aaaae82a32bd2
c17hawke <sunny.c17hawke@gmail.com>
2022-06-04 17:45:30 +0530

    setup updated with more libs

--- init_setup.sh
+++ init_setup.sh
@@ -7,7 +7,7 @@ source activate ./env
 echo [$(date)]: "install requirements"
 pip install -r requirements.txt
 echo [$(date)]: "installing pytorch 1.8.1"
-conda install pytorch==1.8.1 cudatoolkit=11.3 -c pytorch -c conda-forge
+conda install pytorch==1.8.1 cudatoolkit=11.3 -c pytorch -c conda-forge -y
 echo [$(date)]: "END"
 # to remove everything -
 # rm -rf env/ .gitignore conda.yaml README.md .git/
